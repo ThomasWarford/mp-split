09ef49963c094f26bcd7007a26cfde0c6b81d3b8
Thomas Warford <thomasdwarford@hotmail.co.uk>
2025-01-28 17:54:41 +0000

    save REF_stress as non-voigt. Memory saving

--- main.py
+++ main.py
@@ -45,25 +45,25 @@ if __name__ == '__main__':
         for atoms in c:
             atoms.info['REF_energy'] = atoms.get_total_energy()
             atoms.arrays['REF_forces'] = atoms.get_forces()
-            atoms.info['REF_stress'] = atoms.get_stress()
+            atoms.info['REF_stress'] = atoms.get_stress(voigt=False)
         gga_configs.extend(c)
+    write('mptrj-gga.xyz', gga_configs)
+    gga_elements = count_get_all_elements(gga_configs)
+    del gga_configs
+
     ggapu_config_ = [configs for configs, is_ggapu in results if is_ggapu]
     ggapu_configs = []
     for c in ggapu_config_:
         for atoms in c:
             atoms.info['REF_energy'] = atoms.get_total_energy()
             atoms.arrays['REF_forces'] = atoms.get_forces()
-            atoms.info['REF_stress'] = atoms.get_stress()
+            atoms.info['REF_stress'] = atoms.get_stress(voigt=False)
         ggapu_configs.extend(c)
-
-    # Write GGA and GGA+U configs to separate xyz files
-    write('mptrj-gga.xyz', gga_configs)
     write('mptrj-ggapu.xyz', ggapu_configs)
+    ggapu_elements = count_get_all_elements(ggapu_configs)
 
     # add IsolatedAtoms
     e0s = read('isolated_atoms_VASP_PBE.extxyz', ':')
-    gga_elements = count_get_all_elements(gga_configs)
-    ggapu_elements = count_get_all_elements(ggapu_configs)
 
     write('mptrj-gga.xyz', [config for config in e0s if config[0].symbol in gga_elements], append=True)
     write('mptrj-ggapu.xyz', [config for config in e0s if config[0].symbol in ggapu_elements], append=True)
